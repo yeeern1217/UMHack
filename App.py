f90ce7d4f3c7bc73b61ac9b603bbd827723c4772
yeeern1217 <129727499+yeeern1217@users.noreply.github.com>
2023-05-06 22:49:09 +0800

    Update App.py

--- App.py
+++ App.py
@@ -5,6 +5,7 @@ import plotly.graph_objs as go
 import pickle
 import plotly.express as px
 import altair as alt
+import pycaret
 from sklearn.preprocessing import StandardScaler
 
 scaler = StandardScaler()
